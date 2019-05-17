0a2541b7577177c5192980a69b33a449d4c37ff1
AlthalusAvan <mroche7@tcd.ie>
2019-05-17 11:50:40 +0100

    Base functionality version 0.1.0 complete

---
+++ bot.js
@@ -0,0 +1,72 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, token, riotToken } = require('./config.json');
+const fetch = require('node-fetch');
+
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(token);
+
+client.on('message', async message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const ranks = ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grand Master', 'Challenger'];
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'rank') {
+		const summonerName = args.join('');
+		const summonerDataURL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName;
+		
+
+		const getData = async url => {
+			try {
+				const response = await fetch(url, {
+					headers: {
+						'X-Riot-Token': riotToken,
+					},
+				});
+				const json = await response.json();
+				return json;
+			} catch (error) {
+				console.log(error);
+				message.channel.send('There was an error processing the request! Please try again in a few minutes, or contact an admin via #ask-admins if the issue persists!');
+			}
+		};
+
+		getData(summonerDataURL).then(summonerData => {
+			const summonerId = summonerData.id;
+
+			const rankDataURL = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerId;
+
+			getData(rankDataURL).then(rankData => {
+				const formattedTier = rankData[0].tier.charAt(0) + rankData[0].tier.slice(1).toLowerCase();
+				
+
+				let role = message.guild.roles.find(r => r.name === formattedTier);
+
+				let member = message.member;
+
+				if(message.member.roles.has(role.id)) {
+					message.channel.send('You are currently ' + formattedTier + ' ' + rankData[0].rank + '. Role remaining the same!');
+				} else {
+					for (let key in ranks) {
+						let rank = ranks[key];
+						let currRank = message.guild.roles.find(r => r.name === rank);
+						console.log(rank, currRank);
+						if(message.member.roles.has(currRank.id)) {
+							member.removeRole(currRank).catch(console.error);
+						}
+					}
+	
+					member.addRole(role).catch(console.error);
+					message.channel.send('You are currently ' + formattedTier + ' ' + rankData[0].rank + '. Assigning role!');
+				}				
+			});
+		});
+	}
+});
