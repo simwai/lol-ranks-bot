aed5f2837c7ec61f8356b76c19b48e29cd403d72
AlthalusAvan <mroche7@tcd.ie>
2019-05-20 18:22:51 +0100

    Break down into sub-functions

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const { prefix, token, riotToken } = require('./config.json');
 const fetch = require('node-fetch');
 const { URL } = require('url');
 
+const ranks = ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster', 'Challenger'];
 
 client.once('ready', () => {
 	console.log('Ready!');
@@ -14,68 +15,82 @@ client.login(token);
 client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot)	return;
 
-	const ranks = ['Iron', 'Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Master', 'Grandmaster', 'Challenger'];
-
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (command === 'rank') {
-		const summonerName = args.join('');
-		const summonerDataURL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName;
-
-
-		const getData = async url => {
-			try {
-				const response = await fetch(new URL(url), {
-					headers: {
-						'X-Riot-Token': riotToken,
-					},
-				});
-				const json = await response.json();
-				return json;
-			} catch (error) {
-				console.log(error);
-				message.channel.send('There was an error processing the request! Please try again in a few minutes, or contact an admin via #ask-admins if the issue persists!');
-			}
-		};
-
-		const summonerData = await getData(summonerDataURL);
-
-		const rankDataURL = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerData.id;
-
-		getData(rankDataURL).then(rankData => {
-			let soloQueueRankData = null;
-
-			for (let key in rankData) {
-				let currData = rankData[key];
-				if (currData.queueType === 'RANKED_SOLO_5x5') {
-					soloQueueRankData = currData;
-				}
-			}
-
-			if (soloQueueRankData) {
-				const formattedTier = soloQueueRankData.tier.charAt(0) + soloQueueRankData.tier.slice(1).toLowerCase();
-
-				const role = message.guild.roles.find(r => r.name === formattedTier);
-				const member = message.member;
-
-				if(message.member.roles.has(role.id)) {
-					message.channel.send(message.member.toString() + ' - You are currently ' + formattedTier + ' ' + soloQueueRankData.rank + '. You already have that role!');
-				} else {
-					for (let key in ranks) {
-						let rank = ranks[key];
-						let currRank = message.guild.roles.find(r => r.name === rank);
-						if(message.member.roles.has(currRank.id)) {
-							member.removeRole(currRank).catch(console.error);
-						}
-					}
-
-					member.addRole(role).catch(console.error);
-					message.channel.send(message.member.toString() + ' - you are currently ' + formattedTier + ' ' + soloQueueRankData.rank + '. Assigning role!');
-				}
-			} else {
-				message.channel.send(message.member.toString() + ' - can\'t find a Solo Queue rank for that summoner name! Please check your summoner name, or try again later');
-			}
-		});
+	switch (command) {
+	case 'rank':
+		setRoleByRank(message, args);
+		break;
+	default:
+		break;
 	}
 });
+
+async function getData(message, url) {
+	try {
+		const response = await fetch(new URL(url), {
+			headers: {
+				'X-Riot-Token': riotToken,
+			},
+		});
+		const json = await response.json();
+		return json;
+	} catch (error) {
+		console.log(error);
+		message.reply('There was an error processing the request! Please try again in a few minutes, or contact an admin via #ask-admins if the issue persists!');
+	}
+}
+
+async function getSummonerData(message, args) {
+	const summonerName = args.join('');
+	console.log('Getting data for ' + summonerName);
+	const summonerDataURL = 'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + summonerName;
+
+	const summonerData = await getData(message, summonerDataURL);
+
+	return summonerData;
+}
+
+async function setRoleByRank(message, args, summonerData = null) {
+	if (!summonerData) {
+		summonerData = await getSummonerData(message, args);
+	}
+
+	const rankDataURL = 'https://euw1.api.riotgames.com/lol/league/v4/entries/by-summoner/' + summonerData.id;
+
+	getData(message, rankDataURL).then(rankData => {
+		let soloQueueRankData = null;
+
+		for (let key in rankData) {
+			let currData = rankData[key];
+			if (currData.queueType === 'RANKED_SOLO_5x5') {
+				soloQueueRankData = currData;
+			}
+		}
+
+		if (soloQueueRankData) {
+			const formattedTier = soloQueueRankData.tier.charAt(0) + soloQueueRankData.tier.slice(1).toLowerCase();
+
+			const role = message.guild.roles.find(r => r.name === formattedTier);
+			const member = message.member;
+
+			if(message.member.roles.has(role.id)) {
+				message.reply('You are currently ' + formattedTier + ' ' + soloQueueRankData.rank + '. You already have that role!');
+			} else {
+				for (let key in ranks) {
+					let rank = ranks[key];
+					let currRank = message.guild.roles.find(r => r.name === rank);
+					if(message.member.roles.has(currRank.id)) {
+						member.removeRole(currRank).catch(console.error);
+					}
+				}
+
+				member.addRole(role).catch(console.error);
+				message.reply('You are currently ' + formattedTier + ' ' + soloQueueRankData.rank + '. Assigning role!');
+			}
+		} else {
+			message.reply('Can\'t find a Solo Queue rank for that summoner name! Please check your summoner name, or try again later');
+		}
+	});
+}
