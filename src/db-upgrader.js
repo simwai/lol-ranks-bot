7eaea6fc74a53efea180037f9d2e08c6c697ab3e
simwai <16225108+simwai@users.noreply.github.com>
2024-01-11 11:14:30 +0100

    fix(db-upgrader): 🐛 Enhanced property existence checks in DbUpgrader
    This commit refines the logic within the DbUpgrader class to more
    accurately verify and update player properties.
    
    The improvements made include:
    
    - Adjusting the condition to check for the 'authenticated' field in
    player objects by directly assessing the property's existence.
    This change prevents the accidental triggering of database updates
    when the property is absent, avoiding unnecessary write operations.
    
    - Refining the authCode validation to employ a direct regex check against
    the property when present. This eliminates the need for database searches
    and ensures that only authCodes failing the regex pattern are reset,
    preserving valid entries.
    
    - Updating the tier and totalValue property checks to ascertain their
    presence in the player object directly. This modification ensures that
    database updates occur only when these properties are missing,
    thereby maintaining the integrity of existing data.
    
    These enhancements remove the reliance on truthiness checks and
    database searches for property updates, leading to a more efficient
    and accurate database upgrade process by only modifying player objects
    when it is truly warranted.

--- src/db-upgrader.js
+++ src/db-upgrader.js
@@ -1,6 +1,5 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
-const db = require('../players.json')
 
 // Repairs old db files with deprecated db schema
 class DbUpgrader {
@@ -14,27 +13,23 @@ class DbUpgrader {
     let didSomething = false
 
     for (const player of dbJson.players) {
-      if (db.players.find((player) => player.authenticated)) {
+      if ('authenticated' in player) {
         player.auth = player.authenticated
         delete player.authenticated
         didSomething = true
       }
 
-      if (
-        !db.players.find((player) =>
-          player.authCode?.toString().match(/^\d{1,5}$/g)
-        )
-      ) {
+      if (!player.authCode?.toString().match(/^\d{1,5}$/g)) {
         player.authCode = null
         didSomething = true
       }
 
-      if (!db.players.find((player) => player.tier)) {
+      if (!('tier' in player)) {
         player.tier = null
         didSomething = true
       }
 
-      if (!db.players.find((player) => player.totalValue)) {
+      if (!('totalValue' in player)) {
         player.totalValue = null
         didSomething = true
       }
